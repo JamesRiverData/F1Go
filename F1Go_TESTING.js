d87bfb54ca4183b1f089967962abb81c655dcc09
caleb.rankin <caleb.rankin@jamesriver.church>
2026-06-04 13:39:12 -0500

    moved the testing for the post-dated hidden field (fieldHidePopulateWithTextAfterDate) into its own F1Go_TESTING.js place so we don't break other forms when we reference it.

---
+++ F1Go_TESTING.js
@@ -0,0 +1,9 @@
+console.log("Version .01 2026-06-04 at 12:42 pm");
+
+import { populateFieldAfterDateTime, scheduleMultipleFieldPopulations } from './idea/fieldHidePopulateWithTextAfterDate.js';
+
+// Console logs for debugging
+console.log('F1Go.js has been loaded');
+
+globalThis.scheduleMultipleFieldPopulations = scheduleMultipleFieldPopulations;
+globalThis.populateFieldAfterDateTime = populateFieldAfterDateTime;
