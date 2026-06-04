f964e1f3f30ffe254abd97cc0a306bca494f3f7b
caleb.rankin <caleb.rankin@jamesriver.church>
2026-06-04 14:09:47 -0500

    more work on the Testing form...moved fieldHidePopulateWithTextAfterDate.js out of .ideas folder...seemed to have path issues.

--- F1Go_TESTING.js
+++ F1Go_TESTING.js
@@ -1,6 +1,6 @@
 console.log("Version .01 2026-06-04 at 12:42 pm");
 
-import { populateFieldAfterDateTime, scheduleMultipleFieldPopulations } from './idea/fieldHidePopulateWithTextAfterDate.js';
+import { populateFieldAfterDateTime, scheduleMultipleFieldPopulations } from '/fieldHidePopulateWithTextAfterDate.js';
 
 // Console logs for debugging
 console.log('F1Go.js has been loaded');
